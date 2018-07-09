7d69d44cafb5ef7be7777a06d33efce2041322ce
nguyenxuananh <nguyenxuananh22121994@gmail.com>
2018-07-09 10:44:28 +0700

    abc
    em k lấy đc giá trị %

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-document.write ("<h1>Chép phạt</h1>");
+function myFunction(value) {
+	document.getElementById("ketqua").value += +value
+}
+function tinhtoan() {
+	try{
+		var ketqua = eval(document.getElementById("ketqua").value);
+		document.getElementById("ketqua").value = ketqua;
+	}catch(e){
+		document.getElementById("ketqua").value = 'Thằng ngu';
+	}
+}
+function phay() {
+	document.getElementById("ketqua").value += "."
+}	
+function cong() {
+	document.getElementById("ketqua").value += "+"
+}
+function tru() {
+	document.getElementById("ketqua").value += "-"
+}
+function nhan() {
+	document.getElementById("ketqua").value += "*"
+}
+function chia() {
+	document.getElementById("ketqua").value += "/"
+}
+function dellete() {
+	var ketqua = document.getElementById("ketqua").value
+	var newketqua = ketqua.slice(0, -1)
+	document.getElementById("ketqua").value = newketqua;
+}
+function pi() {
+	var soPi = Math.PI;
+	document.getElementById("ketqua").value += soPi;	
+}
+function reset() {
+	document.getElementById("ketqua").value = '';
+}
