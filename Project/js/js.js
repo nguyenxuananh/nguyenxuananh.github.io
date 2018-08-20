76452c0bd465670e4ed682c4689e683b17b6ab91
nguyenxuananh <nguyenxuananh22121994@gmail.com>
2018-08-20 22:45:59 +0700

    asadsa
    dasds

--- Project/js/js.js
+++ Project/js/js.js
@@ -1,32 +1,54 @@
-var number = 1;
-for (i = number; i <= 20; i++) {
-	var newUser = document.createElement("option");
-	newUser.value = i;
-	newUser.innerHTML = i;
-	user.append(newUser);
-}
-$(document).ready(function($) {    
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 90){
-       		$('.menu').addClass('fixed-top')
-       		$('.menu').css('box-shadow', '0px 2px 2px rgba(0,0,0,0.5)')
-       		$('.menu').css('z-index', '10000')
-        }else {
-            $('.menu').removeClass('fixed-top')
-            $('.menu').css('box-shadow', '0px 0px 0px 0px')
-        }
-        if ($(this).scrollTop() > 800) {
-        	$('.back').css('display', 'block')
-        }else {
-        	$('.back').css('display', 'none')
-        }
-   })
+ $(document).ready(function($) {    
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 70){
+      $('.menu').addClass('fixed-top')
+      $('.menu').css('box-shadow', '0px 2px 2px rgba(0,0,0,0.5)')
+      $('.menu').css('z-index', '1000')
+    }else {
+      $('.menu').removeClass('fixed-top')
+      $('.menu').css('box-shadow', '0px 0px 0px 0px')
+    }
+    if ($(this).scrollTop() > 800) {
+      $('.back').css('display', 'block')
+    }else {
+      $('.back').css('display', 'none')
+    }
+  })
 });
-$(document).ready(function($){
-    $(".back").click(function(){
-        $('html').animate({scrollTop:0},1000);
-    });
+ // animate css
+ $(window).scroll(function() {
+   if ($(this).scrollTop() > 400) {
+    $('.wrap').addClass('animated bounceInLeft');
+  }else {
+    $('.wrap').removeClass('animated bounceInLeft');
+  }
+  if ($(this).scrollTop() > 1200) {
+    $('#myTabContent').addClass('animated bounceInRight');
+  }else {
+    $('#myTabContent').removeClass('animated bounceInRight');
+  }
+  if ($(this).scrollTop() > 2110) {
+    $('.container-fluid').addClass('animated zoomIn');
+  }else {
+    $('.container-fluid').removeClass('animated zoomIn');
+  }
+})
+
+ $(document).ready(function($){
+  $(".back").click(function(){
+    $('html').animate({scrollTop:0},1000);
+  });
 });
-$('.messenger').click(function() {
+
+ $('.messenger_a').click(function() {
   $('.messenger_form').toggle();
 })
+
+ var number = 1;
+ for (i = number; i <= 20; i++) {
+  var newUser = document.createElement("option");
+  newUser.value = i;
+  newUser.innerHTML = i;
+  user.append(newUser);
+};
+
